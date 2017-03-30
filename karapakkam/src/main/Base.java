c4984346aa10772fc5dc557042f8ae59917cf3c3
Vinoth Kumar Mahendira <vinothkumarmahendra@gmail.com>
2017-03-30 10:41:12 +0530

    first commit

---
+++ karapakkam/src/main/Base.java
@@ -0,0 +1,81 @@
+package Sample;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+
+public class Base {
+
+	public WebDriver driver =  null ;
+	@BeforeClass
+	public void openBrowser(){
+		String browser= "chrome";
+		System.out.println("I'm in Before suite");
+		if(browser.equals("mozilla")){
+			System.setProperty("webdriver.gecko.driver", 
+					 "browser/geckodriver.exe");
+			driver = new FirefoxDriver();
+		}
+		else if(browser.equals("chrome")){
+			System.setProperty("webdriver.chrome.driver", "browser/chromedriver.exe");
+			driver = new ChromeDriver();
+			System.out.println("dd");
+		}
+		else if(browser.equals("explorer")){
+			System.setProperty("webdriver.ie.driver", "browser/IEDriverServer.exe");
+			driver = new InternetExplorerDriver();
+		}
+		else{
+			System.setProperty("webdriver.gecko.driver", 
+					 "browser/geckodriver.exe");
+			driver = new FirefoxDriver();
+		}
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);		 
+		driver.manage().window().maximize();
+	}
+	
+	@AfterSuite
+	public void closeBrowser(){
+		System.out.println("I'm in After suite");
+		 driver.close();
+	}
+	
+	
+	@AfterClass
+	public void afterC(){
+		System.out.println("I'm in After Class");
+	}
+	@AfterTest
+	public void AfterTest(){
+		System.out.println("I'm in After Test");
+	}
+	
+	@BeforeTest
+	public void openUrl() throws InterruptedException{	
+		
+		System.out.println("Hello base class");
+		 driver.get("http://toolsqa.com/iframe-practice-page");
+		 Thread.sleep(1000);
+		
+	}
+	
+//	
+//	public void pause(int milliseconds){
+//		try {
+//			Thread.sleep(milliseconds);
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//	}
+}
