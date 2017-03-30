ffbfb2d5ca744d3af33f8faaa27c5c67e7bb2beb
Vinoth Kumar Mahendira <vinothkumarmahendra@gmail.com>
2017-03-30 10:51:15 +0530

    second changes

---
+++ karapakkam/src/main/java/automationMind/karapakkam/Gmailtest.java
@@ -0,0 +1,27 @@
+package automationMind.karapakkam;
+
+import org.testng.annotations.BeforeTest;
+
+public class Gmailtest extends Base {	
+
+	@Override
+	@BeforeTest
+	public void openUrl() throws InterruptedException{	
+		
+		//System.out.println("Hello child class");
+		
+		driver.get("http://gmail.com");
+		 Thread.sleep(1000);
+		
+	}
+	
+	public void login(){
+		
+	//	pause(10000);
+	}
+	
+	public void compose(){
+		
+	}
+
+}
